01d6e3aee193381a728cc5025270b95d608f262f
Dachl1590 <113272221+Dachl1590@users.noreply.github.com>
2022-12-04 17:34:27 -0500

    Add files via upload

---
+++ lib/config/configuration.dart
@@ -0,0 +1,23 @@
+class Configuration {
+  static const apiKey =
+      "AIzaSyD_DPfi2lxHUIIdF73zZORFr6rNn5Bzqfo"; // tomado de current_key
+  static const authDomain =
+      "reto3-53fc4.firebaseapp.com"; // tomado de oauth_client
+  static const databaseURL =
+      "https://reto3-53fc4-default-rtdb.firebaseio.com/"; // tomado de firebase_url
+  static const projectId = "reto3-53fc4"; // tomado de project_id
+  static const storageBucket =
+      "reto3-53fc4.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "1049704325400"; //no lo vamos a usar
+  static const appId =
+      "1:1049704325400:web:76623a0262f89a68f1be6d"; // tomado de mobilesdk_app_id
+  static const measurementId = "G-36TBCLQJPG"; //no lo vamos a usar
+}
+
+//apiKey: "AIzaSyD_DPfi2lxHUIIdF73zZORFr6rNn5Bzqfo",
+  //authDomain: "reto3-53fc4.firebaseapp.com",
+  //projectId: "reto3-53fc4",
+  //storageBucket: "reto3-53fc4.appspot.com",
+  //messagingSenderId: "1049704325400",
+  //appId: "1:1049704325400:web:76623a0262f89a68f1be6d",
+  //measurementId: "G-36TBCLQJPG"
